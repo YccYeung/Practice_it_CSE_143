ccd3bac8c0dbddf336c1d365061e59458a41e3df
IGoToSchoolByBus <70379183+YccYeung@users.noreply.github.com>
2023-08-30 13:32:38 -0500

    Create stutter.java

---
+++ CS2_Sections/Section 7 (LinkedList 2)/stutter.java
@@ -0,0 +1,10 @@
+public void stutter() {
+    
+    ListNode current = front;
+    
+    while (current != null) {
+          
+        current.next = new ListNode(current.data, current.next);
+        current = current.next.next;
+    }    
+}
